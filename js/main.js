d2afefb50c041ab072f2c4e5c6a6b4374b64ee63
madbyit <madbyit@bystedts.com>
2022-04-22 14:49:08 +0200

    Add popup on mouse event to portfolio

--- js/main.js
+++ js/main.js
@@ -13,3 +13,21 @@ function getyear()
    console.log('Current year is', currentYear); // 👉️ 2022
    return currentYear;
 }
+
+function popupunjo()
+{
+   var popup = document.getElementById("unjopop");
+   popup.classList.toggle("show");
+}
+
+function popupdiadrom()
+{
+   var popup = document.getElementById("popdiadrom");
+   popup.classList.toggle("show");
+}
+
+function popupesab()
+{
+   var popup = document.getElementById("esabpop");
+   popup.classList.toggle("show");
+}
